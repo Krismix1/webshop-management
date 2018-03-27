033f5c55135e85f4220977c3a679f8729b7afac6
Cristian Betivu <cbetivu@gmail.com>
2018-03-27 20:47:51 +0200

    Project initialization

---
+++ src/main/java/dk/cristi/app/webshop/management/WebshopManagementApplication.java
@@ -0,0 +1,12 @@
+package dk.cristi.app.webshop.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopManagementApplication.class, args);
+	}
+}
