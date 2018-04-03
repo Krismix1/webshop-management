36248e69b7554b44ff36341c7b59cf9bef1a62db
Cristian Betivu <cbetivu@gmail.com>
2018-04-03 23:38:47 +0200

    Update configuration

--- src/main/java/dk/cristi/app/webshop/management/WebShopManagementApplication.java
+++ src/main/java/dk/cristi/app/webshop/management/WebShopManagementApplication.java
@@ -2,10 +2,8 @@ package dk.cristi.app.webshop.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class WebShopManagementApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/dk/cristi/app/webshop/management/configs/WebConfigDev.java
@@ -0,0 +1,15 @@
+package dk.cristi.app.webshop.management.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Profile("dev")
+@Configuration
+public class WebConfigDev implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**"); // Allow CORS for Angular application run as a different application
+    }
+}
