437993347256ad50b45903b4f882de3051fbb6a7
Cristian Betivu <cbetivu@gmail.com>
2018-04-29 13:02:56 +0200

    Init oauth2 authorization

--- src/main/java/dk/cristi/app/webshop/management/WebShopManagementApplication.java
+++ src/main/java/dk/cristi/app/webshop/management/WebShopManagementApplication.java
@@ -2,8 +2,10 @@ package dk.cristi.app.webshop.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class WebShopManagementApplication {
 
 	public static void main(String[] args) {
--- src/main/java/dk/cristi/app/webshop/management/controllers/ProductResource.java
+++ src/main/java/dk/cristi/app/webshop/management/controllers/ProductResource.java
@@ -5,6 +5,8 @@ import dk.cristi.app.webshop.management.models.entities.Product;
 import dk.cristi.app.webshop.management.services.ProductService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +19,7 @@ import java.util.List;
 @RequestMapping("/api/products")
 @Api(tags = {"product"})
 public class ProductResource {
-
+    private static final Logger logger = LoggerFactory.getLogger(ProductResource.class);
     private final ProductService productService;
 
     @Autowired
