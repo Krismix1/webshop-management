cce4541a4ead9c58ffb11914780c919ac952eb3d
Epharas <cris0922@stud.kea.dk>
2018-05-28 02:05:57 +0200

    More properties files and docker deployment

---
+++ src/main/docker/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Wait for config server
+status=$(curl http://configserver:8080/actuator/health | jq -r '.status')
+until [ "$status" = "UP" ]; do
+    echo "Waiting for config server"
+    sleep 1
+    status=$(curl http://configserver:8080/actuator/health | jq -r '.status')
+done
+# Wait for discovery server
+status=$(curl http://discoveryserver:8080/actuator/health | jq -r '.status')
+until [ "$status" = "UP" ]; do
+    echo "Waiting for discovery server"
+    sleep 1
+    status=$(curl http://discoveryserver:8080/actuator/health | jq -r '.status')
+done
+# Wait for gateway server
+status=$(curl http://configserver:8080/actuator/health | jq -r '.status')
+until [ "$status" = "UP" ]; do
+    echo "Waiting for gateway server"
+    sleep 1
+    status=$(curl http://configserver:8080/actuator/health | jq -r '.status')
+done
+echo "Starting management service"
+# Run tomcat
+/usr/local/tomcat/bin/catalina.sh run
